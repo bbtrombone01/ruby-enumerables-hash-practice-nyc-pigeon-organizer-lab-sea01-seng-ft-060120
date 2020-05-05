78b4d384417cf5e70fe256c2330422e9a135293c
steven <bbtrombone@sbcglobal.net>
2020-05-05 20:23:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
+require "pry"
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  new_hash = {}
+  
+  data.each do | big, small|
+    small.each do |key,value|
+    binding.pry
+    end 
+  end
+end 
